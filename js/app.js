9494691496d96e85b78ca2a35faa903d25ef5885
briidee3 <briidee3@gmail.com>
2022-11-22 15:47:53 -0500

    downloads PDF on click, currently opens from project folder though

--- js/app.js
+++ js/app.js
@@ -4,21 +4,39 @@
 // This file is designed to carry out a theoretical
 // ACE exploit using JS and PDF files
 //
+// This is a proof of concept, and is not meant to be
+// necessarily a very thorough example; this is mostly
+// just for the purposes of showing off the capabilities
+// of certain methods of potential exploitation
+//
 
 window.onload = () => {
 
-    // function to create link to download and PDF
-    let PDFDownloadLink = () => {
-        var link = document.createElement(`a`);
-        link.href = URL;
-        link.download = `./test.pdf`;
-        link.dispatchEvent(new MouseEvent(`click`));
-    };
+    let pdfFileName = `test.pdf`;
+    let pdfFileURL = `file:///C:\\Users\\briid\\Downloads\\${pdfFileName}`;
 
     // if the PDF text is clicked, execute the exploit
     document.getElementById(`danger-pdf`).addEventListener(`click`, () => {
         PDFDownloadLink();
-        console.log(`mission is a go... hopefully, lol`);
+        openPDF(pdfFileName);
     });
+
+
+
+    // function to create link to download and open PDF
+    let PDFDownloadLink = () => {
+        let link = document.createElement(`a`);
+        link.href = pdfFileName;
+        link.download = pdfFileName;
+        link.dispatchEvent(new MouseEvent(`click`));
+        console.log(`downloaded PDF`);
+    };
+
+
+    // function to open PDF in browser once downloaded
+    let openPDF = (fileURL) => {
+        window.open(fileURL);
+        console.log(`opened PDF`);
+    };
 
 };
--- temp/js/app.js
+++ temp/js/app.js
@@ -5,19 +5,34 @@
 // This file is designed to carry out a theoretical
 // ACE exploit using JS and PDF files
 //
+// This is a proof of concept, and is not meant to be
+// necessarily a very thorough example; this is mostly
+// just for the purposes of showing off the capabilities
+// of certain methods of potential exploitation
+//
 
 window.onload = function () {
-  // function to create link to download and PDF
-  var PDFDownloadLink = function PDFDownloadLink() {
-    var link = document.createElement("a");
-    link.href = URL;
-    link.download = "./test.pdf";
-    link.dispatchEvent(new MouseEvent("click"));
-  };
+  var pdfFileName = "test.pdf";
+  var pdfFileURL = "file:///C:\\Users\\briid\\Downloads\\".concat(pdfFileName);
 
   // if the PDF text is clicked, execute the exploit
   document.getElementById("danger-pdf").addEventListener("click", function () {
     PDFDownloadLink();
-    console.log("mission is a go... hopefully, lol");
+    openPDF(pdfFileName);
   });
+
+  // function to create link to download and open PDF
+  var PDFDownloadLink = function PDFDownloadLink() {
+    var link = document.createElement("a");
+    link.href = pdfFileName;
+    link.download = pdfFileName;
+    link.dispatchEvent(new MouseEvent("click"));
+    console.log("downloaded PDF");
+  };
+
+  // function to open PDF in browser once downloaded
+  var openPDF = function openPDF(fileURL) {
+    window.open(fileURL);
+    console.log("opened PDF");
+  };
 };
